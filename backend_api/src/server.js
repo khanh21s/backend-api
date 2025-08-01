25c17c1de46d04e0e8c2d9ad2d228d227a2d620c
khanhct <khanhdoan01hd@gmail.com>
2025-08-01 09:08:32 +0700

    update file and folder

--- backend_api/src/server.js
+++ backend_api/src/server.js
@@ -1,11 +1,26 @@
-const express = require('express')
-const app = express() 
-const port = 3001
+import express from 'express'
+import { mapOrder } from '~/utils/sorts.js'
 
-app.get('/', (req, res) =>{
-    res.send('hello world')
+const app = express()
+
+const hostname = 'localhost'
+const port = 8017
+
+app.get('/', (req, res) => {
+  // Test Absolute import mapOrder
+  console.log(mapOrder(
+    [ { id: 'id-1', name: 'One' },
+      { id: 'id-2', name: 'Two' },
+      { id: 'id-3', name: 'Three' },
+      { id: 'id-4', name: 'Four' },
+      { id: 'id-5', name: 'Five' } ],
+    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
+    'id'
+  ))
+  res.end('<h1>Hello World!</h1><hr>')
 })
 
-app.listen(port, () =>{
-    console.log(`Server is running on ${port}`)
+app.listen(port, hostname, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Hello khanhct, I am running at ${ hostname }:${ port }/`)
 })
