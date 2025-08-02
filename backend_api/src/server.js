d9168f8af80883a15ac76efee39702460ba0af24
khanhct <khanhdoan01hd@gmail.com>
2025-08-02 22:34:42 +0700

    updeate author

--- backend_api/src/server.js
+++ backend_api/src/server.js
@@ -22,5 +22,5 @@ app.get('/', (req, res) => {
 
 app.listen(port, hostname, () => {
   // eslint-disable-next-line no-console
-  console.log(`Hello khanhct, I am running at ${ hostname }:${ port }/`)
+  console.log(`Hello khanhct, I am running at http://${ hostname }:${ port }/`)
 })
